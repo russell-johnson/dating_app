de06c4a83c17dd983cfc3407dfa000dcabafc0af
GavMan1995 <gavyncaldwell@gmail.com>
2016-03-04 15:58:04 -0700

    got the address form working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   def update
     if @user.update(user_params)
       redirect_to user_path
-    else 
+    else
       render :edit
     end
   end
@@ -27,13 +27,13 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       redirect_to user_path(@user)
-    else 
+    else
       render :new
     end
   end
 
   def destroy
-    @user.destroy 
+    @user.destroy
     redirect_to root_path
   end
 
