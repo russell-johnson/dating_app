c0517e39bfb81f162928d182852394fa4d267372
Russell Johnson <russellmax86@gmail.com>
2016-03-04 16:26:01 -0700

    created interests features

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -1,5 +1,10 @@
 class InterestsController < ApplicationController
+
+  before_action :user
+  before_action :interest, only: [:edit, :update, :destroy]
+
   def index
+    @interests = @user.interest.all
   end
 
   def show
@@ -8,6 +13,43 @@ class InterestsController < ApplicationController
   def edit
   end
 
+  def update
+      if @interest.update(interest_params)
+        redirect_to user_interests_path(@user, @interest)
+      else
+        render :edit
+    end
+  end
+
   def new
+    @interest = Interest.new
+  end
+
+  def create
+    @interest = @user.interest.new(interest_params)
+    if @interest.save
+      redirect_to user_interests_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @interest.destroy
+    redirect_to user_interests_path(@user)
+  end
+
+  private
+
+  def interest_params
+     params.require(:interest).permit(:name)
+  end
+
+  def user
+      @user = User.find(params[:user_id])
+    end
+
+  def interest
+    @interest = @user.interest.find(params[:id])
   end
 end
