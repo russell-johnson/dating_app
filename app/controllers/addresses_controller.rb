096c8eb52f571d34e79b1d782f99d7b4d866a173
GavMan1995 <gavyncaldwell@gmail.com>
2016-03-04 15:58:04 -0700

    got the address form working

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,13 +1,52 @@
 class AddressesController < ApplicationController
+  before_action :user
+  before_action :address, except: [:index, :create, :new]
+
+
   def index
   end
 
-  def show
+  def edit
+
   end
 
-  def edit
+  def update
+    if @address.update(address_params)
+      redirect_to user_address_path(@user, @address)
+    else
+      render :edit
+    end
   end
 
   def new
+    @address = Address.new
+  end
+
+  def create
+    @address = Address.new(address_params)
+    if @address.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @address.destroy
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:street, :city, :state, :zip)
+  end
+
+  def user
+    @user = User.find(params[:user_id])
+  end
+
+  def address
+    @address = @user.addresses.find(params[:id])
   end
 end
