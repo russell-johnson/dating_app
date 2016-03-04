424ba4947b3321ef61d8001349b617776249a4f1
Garrett Stot <garrettstott@gmail.com>
2016-03-04 15:49:46 -0700

    added links to user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   has_one :address, dependent: :destroy
   has_many :interest, dependent: :destroy
-  has_one :bio, dependent: :destroy 
+  has_one :bios, dependent: :destroy 
 end
